066e796c6a88c4fe63a7ac7dedc7ddf89424536a
MakerSquare Student <student@lab-2.att.net>
2013-06-25 11:51:04 -0500

    Ran through the lessons up until server stuff

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,12 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+p = "This is the NKI"
+n = 21
+j = %w(terrier husky lab)
+template = ERB.new "The value of x is: <%= x + n %>"
+template1 = ERB.new "The value of x is: <%= p %>"
+template2 = ERB.new "The value of x is: <%= j << 'pug' %>"
 puts template.result(binding)
+puts template1.result(binding)
+puts template2.result(binding)
